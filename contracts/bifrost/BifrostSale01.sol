0beff42590a060b3a849c1f6f02da5f677570175
Domenic Crypto <dom@rainbowtoken.finance>
2021-11-29 01:33:25 +0000

    Fixed deposit bug

--- contracts/bifrost/BifrostSale01.sol
+++ contracts/bifrost/BifrostSale01.sol
@@ -329,6 +329,7 @@ contract BifrostSale01 is IBifrostSale01, Context {
         require(!_canceled, "Sale is canceled");
         if (running()) {
             require(_raised.add(amount) <= _hardCap, "This amount would exceed the hard cap");
+            require(_deposited[user].add(amount) <= _max, "Cannot contribute more than the max!");
             require(amount >= _min, "Amount must be above min");
             require(amount <= _max, "Amount must be below max");
             if (_whitelist != address(0)) {
